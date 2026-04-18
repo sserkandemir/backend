b56b11596c78ee5d6d51b65e093fc4d44040849e
sserkandemir <32469181+sserkandemir@users.noreply.github.com>
2026-04-18 15:36:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,35 +7,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// TEST
 app.get("/", (req, res) => {
   res.send("Backend çalışıyor 🚀");
 });
 
+// TEST VIDEO
 app.get("/test-video", async (req, res) => {
   try {
-    const libraryId = "638826"; // BURAYI DEĞİŞTİR
-
-    const url = 'https://video.bunnycdn.com/library/${libraryId}/videos';
-
     const response = await axios.post(
-      url,
+      "https://video.bunnycdn.com/library/638826/videos", // BURAYA KENDİ ID
       {
         title: "Test Video"
       },
       {
         headers: {
-          AccessKey: "fabe0246-85e2-443b-9ce779457106-1949-40fb", // BURAYI DA DEĞİŞTİR
-          "Content-Type": "application/json",
-        },
+          AccessKey: "fabe0246-85e2-443b-9ce779457106-1949-40fb", // BURAYA API KEY
+          "Content-Type": "application/json"
+        }
       }
     );
 
     res.json(response.data);
   } catch (error) {
+    console.log(error.response?.data || error.message);
     res.status(500).json({
-      error: error.response?.data || error.message,
+      error: "Hata oluştu",
+      detail: error.response?.data || error.message
     });
   }
 });
 
-app.listen(3000);
+// PORT
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
